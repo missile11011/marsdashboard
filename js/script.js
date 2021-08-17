be67c813817ac41d9c64bc9c4537dbc0f5db45e9
Misael Reyes <misaelrey90@gmail.com>
2021-08-17 10:25:32 -0500

    api call with varibles

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+let image = document.getElementById("image")
+getPhotos("curiosity","chemcam")
+
+function getPhotos(rover,camera){
+    const imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=1000&camera=${camera}&api_key=dLsK5GbW63DisyWiTiwBNvOBP9tZSyHbghV9wOnL`
+    fetch(imageUrl)
+    .then(response => response.json())
+    .then(imageBlob => {
+    let i =Math.floor(Math.random()*imageBlob.photos.length)
+        console.log(i)
+        
+
+        console.log(image.src);
+        console.log(imageBlob)
+        image.src=imageBlob.photos[i].img_src
+    });
+}
