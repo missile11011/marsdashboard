4e1818b0b829b5f9f9a056ef0ec23c7955624b5d
Misael Reyes <misaelrey90@gmail.com>
2021-08-19 12:40:53 -0500

    rover camera selections only

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let image = document.getElementById("image")
 let apikey = "dLsK5GbW63DisyWiTiwBNvOBP9tZSyHbghV9wOnL"
-getPhotos("curiosity","chemcam")
+
 
 function getPhotos(rover,camera){
     const imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=1000&camera=${camera}&api_key=${apikey}`
@@ -8,17 +8,13 @@ function getPhotos(rover,camera){
     .then(response => response.json())
     .then(imageBlob => {
     let i =Math.floor(Math.random()*imageBlob.photos.length)
-        console.log(i)
-        
-
-        console.log(image.src);
-        console.log(imageBlob)
         image.src=imageBlob.photos[i].img_src
     });
 }
 
 document.getElementById("photos").addEventListener("click", (e)=>{
-    getPhotos("curiosity", "chemcam")
+    let camera = document.getElementById("camera").value
+    getPhotos("curiosity", camera)
 })
 
 
