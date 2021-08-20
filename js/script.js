c464e3eaf7213962afbebc278fea71dad7ec27d3
Misael Reyes <misaelrey90@gmail.com>
2021-08-20 09:11:37 -0500

    photo index num input

--- js/script.js
+++ js/script.js
@@ -2,21 +2,28 @@ let image = document.getElementById("image")
 let apikey = "dLsK5GbW63DisyWiTiwBNvOBP9tZSyHbghV9wOnL"
 
 
-function getPhotos(camera){
+function getPhotos(camera, num){
     const imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=${camera}&api_key=${apikey}`
     fetch(imageUrl)
     .then(response => response.json())
     .then(imageBlob => {
-    console.log(imageBlob)
     let i =Math.floor(Math.random()*imageBlob.photos.length)
-        image.src=imageBlob.photos[i].img_src
-        document.getElementById("length").textContent = imageBlob.photos.length
+        image.src=imageBlob.photos[num].img_src
+        document.getElementById("length").textContent = "number of photos " + imageBlob.photos.length
+        console.log(num)
     });
 }
 
 document.getElementById("photos").addEventListener("click", (e)=>{
     let camera = document.getElementById("camera").value
-    getPhotos(camera)
+    let num = document.getElementById("photoNum").value
+    if (num === ""){
+        getPhotos(camera , "0")
+    }
+    else {
+        getPhotos(camera , num-1)
+    }
+    
 })
 
 
